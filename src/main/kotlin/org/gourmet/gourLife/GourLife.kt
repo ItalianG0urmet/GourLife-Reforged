e134abad2ba2b0af81937349b5f7d3eef0b6da70
gourmet <insogna_@outlook.it>
2025-06-08 12:39:48 +0200

    Code cleanup + new Logger system

--- src/main/kotlin/org/gourmet/gourLife/GourLife.kt
+++ src/main/kotlin/org/gourmet/gourLife/GourLife.kt
@@ -9,63 +9,70 @@ import org.gourmet.gourLife.events.KillEvent
 import org.gourmet.gourLife.events.LeaveEvent
 import org.gourmet.gourLife.jsonManager.JsonDataLoader
 import org.gourmet.gourLife.placeholder.PlaceHolderHearts
+import org.gourmet.gourLife.utils.Logger
 import revxrsal.commands.bukkit.BukkitLamp
 
 class GourLife : JavaPlugin() {
 
     companion object {
-        lateinit var instance: GourLife
-            private set
-        lateinit var jsonDataLoader: JsonDataLoader
-            private set
+        lateinit var instance: GourLife private set
+        lateinit var jsonDataLoader: JsonDataLoader private set
     }
 
     override fun onEnable() {
         instance = this
         saveDefaultConfig()
-        logger.info("GourLife starting...")
+        Logger.info("GourLife starting...")
 
-        try {
-            jsonDataLoader = JsonDataLoader(this)
-            jsonDataLoader.loadPlayerData()
-        } catch (e: Exception) {
-            logger.severe("Error: ${e.message}")
-            Bukkit.getPluginManager().disablePlugin(this)
-            return
-        }
-
-
+        initJsonData()
         placeHolderInit()
-        registerEvents()
+        registerEventsAndCommands()
 
-
-        val handler = BukkitLamp.builder(this).build()
-        handler.register(
-            LifeCMD
-        )
-
-        logger.info("GourLife started!")
+        Logger.info("GourLife started!")
     }
 
     override fun onDisable() {
         saveDefaultConfig()
         jsonDataLoader.savePlayerData()
-        logger.info("GourLife disabled")
+        Logger.info("GourLife disabled")
+    }
+
+    /* Impl */
+    private fun initJsonData(){
+        try {
+            jsonDataLoader = JsonDataLoader(this)
+            jsonDataLoader.loadPlayerData()
+        } catch (e: Exception) {
+            Logger.warning("Error: ${e.message}")
+            Bukkit.getPluginManager().disablePlugin(this)
+            return
+        }
     }
 
     private fun placeHolderInit() {
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
-            logger.severe("Missing PlaceHolderAPI")
+            Logger.warning("Missing PlaceHolderAPI")
             Bukkit.getPluginManager().disablePlugin(this)
             return
         }
         PlaceHolderHearts().register()
     }
 
-    private fun registerEvents() {
+    private fun registerEventsAndCommands() {
+
+        //Commands
         val pluginManager = Bukkit.getPluginManager()
-        listOf(DeathEvent(), KillEvent(), LeaveEvent(), JoinEvent()).forEach { event ->
-            pluginManager.registerEvents(event, this)
-        }
+        listOf(
+            DeathEvent(),
+            KillEvent(),
+            LeaveEvent(),
+            JoinEvent()
+        ).forEach { event -> pluginManager.registerEvents(event, this) }
+
+        //Events
+        val handler = BukkitLamp.builder(this).build()
+        handler.register(
+            LifeCMD
+        )
     }
 }
